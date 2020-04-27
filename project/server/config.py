b5d7c6bd983a8ae7aa6caa3b5fa5828b8be276ff
Leon Morten Richter <31622033+M0r13n@users.noreply.github.com>
2020-04-27 15:00:46 +0200

    bootstrap sux

--- project/server/config.py
+++ project/server/config.py
@@ -19,7 +19,7 @@ class BaseConfig(object):
     SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # set optional bootswatch theme
-    FLASK_ADMIN_SWATCH = 'Cyborg'
+    #FLASK_ADMIN_SWATCH = 'Cyborg'
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
     CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", REDIS_URL)
     CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND_URL", REDIS_URL)
