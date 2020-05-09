be94ca972db4001f3c174f2cde4ec2d3753e3fb9
Leon Morten Richter <31622033+M0r13n@users.noreply.github.com>
2020-05-09 12:40:58 +0200

    slightly improve the js

--- project/client/static/scripts/script.js
+++ project/client/static/scripts/script.js
@@ -1,105 +1,133 @@
-// -----------------------------------------------
-// -----------------  Header  --------------------
-// -----------------------------------------------
-document.getElementById("menu").addEventListener("click", function () {
+/*
+* Constants
+* */
+
+const radios = document.getElementsByName('color');
+
+
+/*
+* Global Function
+* */
+
+/**
+ * Return URLSearchParams object for the current URL.
+ * @returns {URLSearchParams}
+ */
+function getURLSearchParams() {
+    const paramsString = window.location.search;
+    return new URLSearchParams(paramsString);
+}
+
+/**
+ * Search Funtion.
+ *
+ * TODO: For stuff like this it is nicer to use a URL query string instead of an url.
+ */
+function appendURL() {
+    let input = document.getElementById("Search");
+    if (input.value.length === 0) return;
+    input = input.value.split(" ").join("$");
+    window.location.href = "/search/" + input + "/"
+}
+
+/*
+ * Radio
+ * */
+document.getElementById("menu").addEventListener("click", () => {
     document.getElementById("nav").classList.toggle("header__list--in")
-}, false)
+}, false);
 
 
-// -----------------------------------------------
-// -----------------   FAQ    --------------------
-// -----------------------------------------------
-if (window.location.pathname == "/faq") {
+/*
+* FAQ
+* */
+if (window.location.pathname === "/faq") {
     // Suchleiste
-    document.getElementById("Search").addEventListener("keyup", function() {
+    document.getElementById("Search").addEventListener("keyup", () => {
         filterFunction()
-    }, false)
+    }, false);
 
     // URL Queries verarbeiten
-    var query = window.location.search.substring(1)
-    questionID = query.split("q=")[1]
-    
-    // Eventlistener an FAQ Fragen appenden und ausführen, wenn entsprechende Queries in URL sind
-    e = document.getElementById("FaqList").getElementsByClassName("faq__item")
-    for (let i = 0; i < e.length; i++) {
-        e[i].addEventListener("click", function() {
-            e[i].classList.toggle("collapsed")
-        }, false)
+    const params = getURLSearchParams(),
+        questionID = params.get('q');
 
-        if(!isNaN(questionID) && parseInt(questionID) >= 0 && parseInt(questionID) < e.length) {
-            e[i].style.display = "none"
-            f = document.getElementById("FaqList").getElementsByTagName("h3")
-            for (let j = 0; j < f.length; j++) {
-                f[j].style.display = "none"
-            }
-            if(questionID == i) {
-                e[i].style.display = ""
-                e[i].classList.toggle("collapsed")
-            }
-        }
-        
-    }
-    // Funktion die Texte und Überschriften nach den eingegebenen Wörtern untersucht
-    function filterFunction() {
-        var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById("Search");
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("FaqList");
-        h3 = ul.getElementsByClassName("faq__subheading")
-        console.log(h3)
-        for (let j = 0; j < h3.length; j++) {
-            h3[j].style.display = "none";
-            if (input.value == "") {
-                h3[j].style.display = "";
-            }
-        }
-        li = ul.querySelectorAll("li.faq__item");
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("span")[0];
-            div = li[i].getElementsByTagName("div")[0];
-            txtValue = a.textContent || a.innerText;
-            txtValuediv = div.innerHTML;
-            if ((txtValue.toUpperCase().indexOf(filter) > -1) || 
-                (txtValuediv.toUpperCase().indexOf(filter) > -1)) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
+    // Eventlistener an FAQ Fragen appenden und ausführen, wenn entsprechende Queries in URL sind
+    questions = document.getElementById("FaqList").getElementsByClassName("faq__item");
+    let i = 0;
+    for (const question of questions) {
+        i++;
+        question.addEventListener("click", () => {
+            question.classList.toggle("collapsed")
+        }, false);
+
+        if (!isNaN(questionID) && parseInt(questionID) >= 0 && parseInt(questionID) < questions.length) {
+            question.style.display = "none";
+            const questionTitles = document.getElementById("FaqList").getElementsByTagName("h3");
+            questionTitles.forEach((title) => {
+                title.style.display = "none";
+            });
+            if (questionID === i.toString()) {
+                question.style.display = "";
+                question.classList.toggle("collapsed")
             }
         }
     }
 }
 
+// Funktion die Texte und Überschriften nach den eingegebenen Wörtern untersucht
+function filterFunction() {
+    const input = document.getElementById("Search"),
+        filter = input.value.toUpperCase(),
+        questionList = document.getElementById("FaqList"),
+        questionHeadings = questionList.getElementsByClassName("faq__subheading");
+
+    let li, a, txtValue, div, txtValuediv;
+
+    for (const heading of questionHeadings) {
+        heading.style.display = "none";
+        if (input.value === "") {
+            heading.style.display = "";
+        }
+    }
+
+
+    li = questionList.querySelectorAll("li.faq__item");
+    li.forEach((l, i) => {
+
+        a = l.getElementsByTagName("span")[0];
+        div = l.getElementsByTagName("div")[0];
+        txtValue = a.textContent || a.innerText;
+        txtValuediv = div.innerHTML;
+        if ((txtValue.toUpperCase().indexOf(filter) > -1) ||
+            (txtValuediv.toUpperCase().indexOf(filter) > -1)) {
+            l.style.display = "";
+        } else {
+            l.style.display = "none";
+        }
+    });
+}
 
 
 // Das ist die Suche für Geräte aus dem Shop-Bereich
-if (window.location.pathname == "/manufacturers" || window.location.pathname.substring(0, 7) == "/search") {
-    console.log('mau')
-    document.getElementById("Submit").addEventListener("click", function(){
+if (window.location.pathname === "/manufacturers" || window.location.pathname.substring(0, 7) === "/search") {
+    document.getElementById("Submit").addEventListener("click", () => {
         appendURL()
-    }, false)
-    document.getElementById("Search").addEventListener("keypress", function(e) {
-        if (e.key == "Enter") {
+    }, false);
+    document.getElementById("Search").addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
             appendURL()
         }
-    } ,false)
-    function appendURL() {
-        input = document.getElementById("Search")
-        if (input.value.length == 0) return
-        input = input.value.split(" ").join("$")
-        window.location.href= "/search/" + input + "/"
-    }
+    }, false);
 }
 
-
-
-var radios = document.getElementsByName('color')
-for (var i = 0, length = radios.length; i < length; i++) {
-    if (radios[i].checked) {
-        document.getElementById("ColorName").innerHTML = "Aktuelle Farbauswahl: " + radios[i].value.replace("_", " ")
+for (let radio of radios) {
+    if (radio.checked) {
+        document.getElementById("ColorName").innerHTML = "Aktuelle Farbauswahl: " + radio.value.replace("_", " ")
     }
-    radios[i].addEventListener("change", function() {
+    radio.addEventListener("change", () => {
         if (this.checked) {
             document.getElementById("ColorName").innerHTML = "Aktuelle Farbauswahl: " + this.value.replace("_", " ")
         }
     }, false)
+
 }
