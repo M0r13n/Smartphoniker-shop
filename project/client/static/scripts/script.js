57a202e29a9be0039a376a235156517499fe31f0
Leon Morten Richter <31622033+M0r13n@users.noreply.github.com>
2020-05-09 14:01:14 +0200

    just my 2 cents

--- project/client/static/scripts/script.js
+++ project/client/static/scripts/script.js
@@ -1,8 +1,3 @@
-/* Constants */
-const radios = document.getElementsByName('color');
-
-
-/* Global Functions */
 /**
  * Return URLSearchParams object for the current URL.
  * @returns {URLSearchParams}
@@ -12,6 +7,19 @@ function getURLSearchParams() {
     return new URLSearchParams(paramsString);
 }
 
+/**
+ * Type safe conversion from string to (positive) integer.
+ * Returns -1 on error.
+ * @param str
+ * @returns {number}
+ */
+const toInt = (str) => {
+    if (str === null || isNaN(str)) {
+        return -1;
+    } else {
+        return parseInt(str);
+    }
+};
 
 /**
  * Search Funtion.
@@ -26,6 +34,21 @@ function appendURL() {
 }
 
 /**
+ * Hide a given element by setting it's display attr to none.
+ */
+function hide(elem) {
+    elem.style.display = "none"
+}
+
+/**
+ * Show a given element by setting it's display attr.
+ */
+function show(elem) {
+    elem.style.display = "";
+}
+
+/**
+ * WTF is this mess?
  * TODO: This method is way to complex and unreadable. Rewrite it.
  */
 function filterFunction() {
@@ -37,80 +60,31 @@ function filterFunction() {
     let li, a, txtValue, div, txtValuediv;
 
     for (const heading of questionHeadings) {
-        heading.style.display = "none";
-        if (input.value === "") {
-            heading.style.display = "";
-        }
+        input.value.length > 0 ? hide(heading) : show(heading);
     }
 
     li = questionList.querySelectorAll("li.faq__item");
     li.forEach((l) => {
-
         a = l.getElementsByTagName("span")[0];
         div = l.getElementsByTagName("div")[0];
         txtValue = a.textContent || a.innerText;
         txtValuediv = div.innerHTML;
         if ((txtValue.toUpperCase().indexOf(filter) > -1) ||
             (txtValuediv.toUpperCase().indexOf(filter) > -1)) {
-            l.style.display = "";
+            show(l);
         } else {
-            l.style.display = "none";
+            hide(l);
         }
     });
 }
 
-/* JS for each page */
 const radioJS = () => {
+    const radios = document.getElementsByName('color');
     document.getElementById("menu").addEventListener("click", () => {
         document.getElementById("nav").classList.toggle("header__list--in")
     }, false);
-};
 
-const faqJS = () => {
-    // Suchleiste
-    document.getElementById("Search").addEventListener("keyup", () => {
-        filterFunction()
-    }, false);
-
-    // URL Queries verarbeiten
-    const params = getURLSearchParams(),
-        questionID = params.get('q');
-
-    // Eventlistener an FAQ Fragen appenden und ausführen, wenn entsprechende Queries in URL sind
-    const questions = document.getElementById("FaqList").getElementsByClassName("faq__item");
-    let i = 0;
-    for (const question of questions) {
-        i++;
-        question.addEventListener("click", () => {
-            question.classList.toggle("collapsed")
-        }, false);
-
-        if (!isNaN(questionID) && parseInt(questionID) >= 0 && parseInt(questionID) < questions.length) {
-            question.style.display = "none";
-            const questionTitles = document.getElementById("FaqList").getElementsByTagName("h3");
-            questionTitles.forEach((title) => {
-                title.style.display = "none";
-            });
-            if (questionID === i.toString()) {
-                question.style.display = "";
-                question.classList.toggle("collapsed")
-            }
-        }
-    }
-};
-
-const searchJS = () => {
-    if (window.location.pathname === "/manufacturers" || window.location.pathname.substring(0, 7) === "/search") {
-        document.getElementById("Submit").addEventListener("click", () => {
-            appendURL()
-        }, false);
-        document.getElementById("Search").addEventListener("keypress", (e) => {
-            if (e.key === "Enter") {
-                appendURL()
-            }
-        }, false);
-    }
-    for (let radio of radios) {
+    for (const radio of radios) {
         if (radio.checked) {
             document.getElementById("ColorName").innerHTML = "Aktuelle Farbauswahl: " + radio.value.replace("_", " ")
         }
@@ -119,7 +93,49 @@ const searchJS = () => {
                 document.getElementById("ColorName").innerHTML = "Aktuelle Farbauswahl: " + this.value.replace("_", " ")
             }
         }, false)
+
     }
+};
+
+const faqJS = () => {
+    document.getElementById("Search").addEventListener("keyup", () => {
+        filterFunction()
+    }, false);
+
+    const params = getURLSearchParams(),
+        questionID = toInt(params.get('q')),
+        questions = document.getElementById("FaqList").getElementsByClassName("faq__item"),
+        questionTitles = document.getElementById("FaqList").getElementsByTagName("h3");
+
+    let i = 0;
+    for (const question of questions) {
+        i++;
+        question.addEventListener("click", () => {
+            question.classList.toggle("collapsed")
+        }, false);
+
+        if (questionID > 0) {
+            hide(question);
+        }
+        if (questionID === i) {
+            for (const title of questionTitles) {
+                hide(title);
+            }
+            show(question);
+            question.classList.toggle("collapsed");
+        }
+    }
+};
+
+const searchJS = () => {
+    document.getElementById("Submit").addEventListener("click", () => {
+        appendURL()
+    }, false);
+    document.getElementById("Search").addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            appendURL()
+        }
+    }, false);
 };
 
 /* Register event listeners */
@@ -127,7 +143,7 @@ radioJS();
 
 if (window.location.pathname === "/faq") {
     faqJS();
-} else if (window.location.pathname === "/manufacturers" || window.location.pathname.substring(0, 7) === "/search") {
+} else if (window.location.pathname === "/manufacturers") {
     searchJS();
 }
 
