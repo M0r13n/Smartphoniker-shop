0ae1d1f81070659daa2cb3ae5242cd68e333efaa
Tobias Pörtner <me@tobiaspoertner.com>
2020-05-17 13:35:28 +0200

    added function to calculate the price in real time

--- project/client/static/scripts/script.js
+++ project/client/static/scripts/script.js
@@ -34,12 +34,12 @@ function $(id) {
  * Search Funtion.
  * @param {string}
  */
-function appendURL(path = "/search/") {
+function appendURL(path = '/search/') {
     const origin = window.location.origin;
     let url = new URLSearchParams(origin + path);
-    let input = $("Search");
+    let input = $('Search');
     if (input.value.length === 0) return;
-    url.searchParams.append("search", input);
+    url.searchParams.append('search', input);
     window.location.href = url;
 }
 
@@ -47,14 +47,14 @@ function appendURL(path = "/search/") {
  * Hide a given element by setting it's display attr to none.
  */
 function hide(elem) {
-    elem.style.display = "none"
+    elem.style.display = 'none'
 }
 
 /**
  * Show a given element by setting it's display attr.
  */
 function show(elem) {
-    elem.style.display = "";
+    elem.style.display = '';
 }
 
 /**
@@ -65,15 +65,15 @@ function show(elem) {
  */
 const extractPlainText = (htmlObject, query = false) => {
     let toBeSearched = query ? htmlObject.querySelectorAll(query)[0] : htmlObject;
-    return toBeSearched.textContent.toLowerCase() || toBeSearched.innerText.toLowerCase() || "";
+    return toBeSearched.textContent.toLowerCase() || toBeSearched.innerText.toLowerCase() || '';
 }
 
 /**
  * filters Questions and Answers on the FAQ Page
  */
 function filterFunction() {
-    const input = $("Search").value.toLowerCase();
-    const questionHeadings = $("FaqList").getElementsByClassName("faq__subheading");
+    const input = $('Search').value.toLowerCase();
+    const questionHeadings = $('FaqList').getElementsByClassName('faq__subheading');
 
     let question;
     let answer;
@@ -82,10 +82,10 @@ function filterFunction() {
         input.length > 0 ? hide(heading) : show(heading);
     }  
 
-    let list = $("FaqList").querySelectorAll("li.faq__item");
+    let list = $('FaqList').querySelectorAll('li.faq__item');
     for (const item of list) {
-        question = extractPlainText(item, "span.faq__question");
-        answer = extractPlainText(item, "div.faq__answer");
+        question = extractPlainText(item, 'span.faq__question');
+        answer = extractPlainText(item, 'div.faq__answer');
 
         if ((question.indexOf(input) > -1) || (answer.indexOf(input) > -1)) {
             show(item);
@@ -99,20 +99,20 @@ function filterFunction() {
  * filters questions with user-input 
  */
 const faqJS = () => {
-    $("Search").addEventListener("keyup", () => {
+    $('Search').addEventListener('keyup', () => {
         filterFunction()
     }, false);
 
     const params = getURLSearchParams(),
         questionID = toInt(params.get('q')),
-        questions = $("FaqList").getElementsByClassName("faq__item"),
-        questionTitles = $("FaqList").getElementsByTagName("h3");
+        questions = $('FaqList').getElementsByClassName('faq__item'),
+        questionTitles = $('FaqList').getElementsByTagName('h3');
 
     let i = 0;
     for (const question of questions) {
         i++;
-        question.addEventListener("click", () => {
-            question.classList.toggle("collapsed")
+        question.addEventListener('click', () => {
+            question.classList.toggle('collapsed')
         }, false);
 
         if (questionID > 0) {
@@ -123,7 +123,7 @@ const faqJS = () => {
                 hide(title);
             }
             show(question);
-            question.classList.toggle("collapsed");
+            question.classList.toggle('collapsed');
         }
     }
 };
@@ -132,21 +132,50 @@ const faqJS = () => {
  * eventlisteners for device-search 
  */
 const searchJS = () => {
-    $("Submit").addEventListener("click", () => {
+    $('Submit').addEventListener('click', () => {
         appendURL()
     }, false);
-    $("Search").addEventListener("keypress", (e) => {
-        if (e.key === "Enter") {
+    $('Search').addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
             appendURL()
         }
     }, false);
 };
 
 /**
+ * returns array of values of elements selected by a css query
+ * @param {string}
+ * @returns {Array}
+ */
+const getValues = (selector) => {
+    var elements = document.querySelectorAll(selector);
+    return [].map.call(elements, el => toInt(el.value));
+}
+
+/**
  * calculate total on modell.html 
  */
 const totalJS = () => {
+    const repairs = document.getElementsByName('repair');
 
+    const calculateSum = () => {
+        let checkedRepairPrices = getValues('input[name="repair"]:checked');
+
+        if (checkedRepairPrices.length > 1) {
+            let cheapest = Math.min(...checkedRepairPrices);
+            let preDiscount = checkedRepairPrices.reduce((sum, x) => sum + x);
+            let total = preDiscount - cheapest + (cheapest * 0.8);
+            $('Total').innerHTML = '<s>' + toInt(preDiscount) + '</s> ' + toInt(total);
+        } else {
+            $('Total').innerHTML = checkedRepairPrices[0] ?? 0;
+        }
+    }
+    
+    for (const repair of repairs) {
+        repair.addEventListener('change', () => {
+            calculateSum()
+        }, false);
+    }
 }
 
 /**
@@ -157,33 +186,33 @@ const colorJS = () => {
 
     for (const radio of radios) {
         if (radio.checked) {
-            $("ColorName").innerHTML = "Aktuelle Farbauswahl: " + radio.id.replace("_", " ")
+            $('ColorName').innerHTML = 'Aktuelle Farbauswahl: ' + radio.id.replace('_', ' ')
         }
-        radio.addEventListener("change", () => {
+        radio.addEventListener('change', () => {
             if (radio.checked) {
-                $("ColorName").innerHTML = "Aktuelle Farbauswahl: " + radio.id.replace("_", " ")
+                $('ColorName').innerHTML = 'Aktuelle Farbauswahl: ' + radio.id.replace('_', ' ')
             }
-        }, false)
+        }, false);
     }
 }
 
 const main = () => {
     /* mobile Navigation */
-    $("menu").addEventListener("click", () => {
-        $("nav").classList.toggle("header__list--in")
+    $('menu').addEventListener('click', () => {
+        $('nav').classList.toggle('header__list--in')
     }, false);
 
-    /* I know that switch statements have bad performance, 
-     * but I think they have great readability and the 
-     * default option makes sense in this case
+    /* I know that switch statements have bad stigma, 
+     * but I think they have great readability & are more maintanable
+     * and the default case makes sense here
      */
     switch (window.location.pathname) {
-        case "/faq":
+        case '/faq':
             faqJS();
             break;
 
-        case "/shop":
-        case "/search":
+        case '/shop':
+        case '/search':
             searchJS();
             break;
 
